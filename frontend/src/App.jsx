c6a5b656cb9464b4a91fa2d878ac77d90041eb8a
adnasarvan <adnaishere@gmail.com>
2026-05-12 22:59:22 +0200

    feat: setup Vite, React environment and add .gitignore

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BrowsePage from './pages/BrowsePage';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowsePage />
+    </div>
+  );
+}
+
+export default App;
