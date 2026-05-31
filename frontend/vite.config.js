bbf7551ece4caa9b460a0611ad5c0e621190a5a7
adnasarvan <adnaishere@gmail.com>
2026-05-31 16:23:24 +0200

    fix:json server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://backend-service-1024177687549.europe-west3.run.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+}
