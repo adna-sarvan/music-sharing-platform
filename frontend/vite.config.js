8b3b56a083880c6ff4680dde4f645b799c23f6c1
adnasarvan <adnaishere@gmail.com>
2026-05-31 17:12:02 +0200

    fix: vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-export default {
-  server: {
-    proxy: {
-      '/songs': 'https://backend-service-1024177687549.europe-west3.run.app'
-    }
-  }
-}
